4869bcc410042dc30d79de1ea3a4e87498f56fe7
Ingvar Suvorin <ingvarr2104@gmail.com>
2022-11-01 22:59:45 +0000

    Moved the cmd line input into index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,24 @@
-const cmdLineArg = process.argv.slice(2);
 const request = require('request');
   
-request('https://api.thecatapi.com/v1/breeds/search', (error, response, body) => {
-    
-  if (error) {
-    console.log(error);
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`${cmdLineArg} not located.`); //Receiving: Siberian not located.
-    return;
-  }
-
-  console.log(data[0]['description']);
-  console.log(typeof data);
-});
-
+    if (error) {
+      callback(error);
+      return;
+    }
   
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`${breedName} not located.`);
+      //console.log(`${cmdLineArg} not located.`); //Receiving: Siberian not located.
+      return;
+    }
+    callback(error, data[0].description);
+  // console.log(data[0]['description']);
+  // console.log(typeof data);
+  });
+};
+
+
+module.exports = { fetchBreedDescription };
